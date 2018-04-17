27113e1f612dbdd489bd93bc085583d76528a6e4
nick-brightside <pusher08@rambler.ru>
2018-04-17 23:43:44 +0300

    Added SQL script for creating the database

---
+++ SpendingTrackerDB.sql
@@ -0,0 +1,28 @@
+﻿create database SpendingTracker;
+use SpendingTracker;
+
+drop table categories;
+drop table spendings;
+drop table users;
+
+create table users
+(
+	user_id int primary key,
+	user_name varchar(40)
+);
+
+create table spendings
+(
+	spending_id int primary key identity(1, 1) not null,
+	user_id int not null,
+	amount float,
+	dt datetime,
+	foreign key (user_id) references users(user_id)
+);
+
+create table categories
+(
+	category_name varchar(40),
+	spending_id int identity(1, 1) not null, 
+	foreign key (spending_id) references spendings(id)
+);
