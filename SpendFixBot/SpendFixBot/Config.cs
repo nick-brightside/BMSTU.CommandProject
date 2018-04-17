35cabd90de624011003e413b71a3a3520f1bdbca
nilke1995 <brekhin@bk.ru>
2018-04-18 02:59:31 +0300

    Create a base for the telegram bot

---
+++ SpendFixBot/SpendFixBot/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendFixBot
+{
+    class Config
+    {
+        public const string API_KEY = "570864095:AAEKDDKwjrezNMBm5j2eOvRZQsgTTQRwzBc";
+    }
+}
