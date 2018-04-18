72e93efeaed82173c52225fd0eec437deabaa4f6
nilke1995 <brekhin@bk.ru>
2018-04-18 04:27:33 +0300

    added a methods for work with telegram API

--- SpendFixBot/SpendFixBot/Program.cs
+++ SpendFixBot/SpendFixBot/Program.cs
@@ -3,13 +3,79 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace SpendFixBot
 {
     class Program
     {
+        private static readonly Telegram.Bot.TelegramBotClient Bot = new TelegramBotClient("570864095:AAEKDDKwjrezNMBm5j2eOvRZQsgTTQRwzBc");
+
         static void Main(string[] args)
         {
+            Bot.OnMessage += BotOnMessageReceived;
+
+            var me = Bot.GetMeAsync().Result;
+            Console.Title = me.Username;
+
+            Bot.StartReceiving();
+            Console.ReadLine();
+            Bot.StopReceiving();
+        }
+
+        private static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        {
+            var message = messageEventArgs.Message;
+
+            if (message == null || message.Type != MessageType.TextMessage)
+                return;
+
+            if (message.Text.StartsWith("/help"))
+            {
+                await Bot.SendTextMessageAsync(message.Chat.Id, SpendFixBot.Constants.HELP_MESSAGE);
+            }
+
+            var keyboardTypeOfOutput = new Telegram.Bot.Types.ReplyMarkups.InlineKeyboardMarkup();
+            keyboardTypeOfOutput.InlineKeyboard = new Telegram.Bot.Types.InlineKeyboardButtons.InlineKeyboardButton[][]
+            {
+                    new Telegram.Bot.Types.InlineKeyboardButtons.InlineKeyboardButton[]
+                    {
+                        new KeyboardButton("по категориям"),
+                        new KeyboardButton("по датам"),
+                    },
+            };
+
+            if (message.Text.StartsWith("/spending"))
+            {
+                Bot.OnCallbackQuery += (object sc, Telegram.Bot.Args.CallbackQueryEventArgs ev) =>
+                {
+                    if (ev.CallbackQuery.Data == "по категориям")
+                    {
+                        // In this plase will be used API to work with the database(SelectRowsByCategory) 
+                        Bot.AnswerCallbackQueryAsync(ev.CallbackQuery.Id);
+                    }
+                    if (ev.CallbackQuery.Data == "по датам")
+                    {
+                        // In this plase will be used API to work with the database(SelectRowsByDates)
+                        Bot.AnswerCallbackQueryAsync(ev.CallbackQuery.Id);
+                    }
+                };
+                await Bot.SendTextMessageAsync(message.Chat.Id, "Выбери вид отчетности", replyMarkup: keyboardTypeOfOutput);
+            }
+
+            if (message.Text.StartsWith("/start"))
+            {
+                // In this plase will be used API to work with the database(InsertUser) 
+                await Bot.SendTextMessageAsync(message.Chat.Id, $"Привет, {message.Chat.FirstName}" + SpendFixBot.Constants.START_MESSAGE);
+            }
+            if (message.Text.StartsWith("/delete"))
+            {
+                // In this plase will be used API to work with the database(DeleteUser) 
+                await Bot.SendTextMessageAsync(message.Chat.Id, SpendFixBot.Constants.DELETE_DONE);
+            }
         }
     }
 }
